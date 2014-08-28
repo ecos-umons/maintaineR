4ae1767485f1458fa2c447ee2191bfbaaf0131d5
Maëlick Claes <himself@maelick.net>
2014-08-28 15:41:43 +0200

    Dependency graphs fix

--- maintaineR/R/render.R
+++ maintaineR/R/render.R
@@ -1,4 +1,7 @@
-RenderSankey <- function(graph) {
+RenderSankey <- function(graph, id=NULL) {
+  if (is.null(id)) {
+    id <- basename(tempfile())
+  }
   E(graph)$value <- 1
   edgelist <- get.data.frame(graph)
   colnames(edgelist) <- c("source","target","value")
@@ -13,18 +16,23 @@ RenderSankey <- function(graph) {
                  layout=32,
                  width=960,
                  height=20*length(V(graph)))
-  sankeyPlot$print(chartId='sankey')
+  sankeyPlot$print(chartId=id)
 }
 
-RenderGraph <- function(graph) {
+RenderGraph <- function(graph, id=NULL) {
+  if (is.null(id)) {
+    id <- basename(tempfile())
+  }
   nodes <- data.frame(name=V(graph)$name, group=V(graph)$group)
   edges <- as.data.frame(get.edgelist(graph, names=FALSE) - 1)
   colnames(edges) <- c("source", "target")
   edges <- edges[with(edges, order(source)), ]
   edges$value <- 1
+  cat(sprintf("<div id=\"%s\">", id))
   d3ForceNetwork(Links=edges, Nodes=nodes,
                  Source="source", Target="target",
                  Value="value", NodeID="name",
                  Group="group", width=800, height=600,
-                 opacity=0.9, parentElement="#depsgraph", zoom=TRUE)
+                 opacity=0.9, parentElement=sprintf("#%s", id), zoom=TRUE)
+  cat("</div>")
 }
