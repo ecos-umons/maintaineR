8b368a5e4bf14e46932da40a11fa73623feaccb6
Maëlick Claes <himself@maelick.net>
2014-09-18 14:14:50 +0200

    StateData function removed

--- R/state.R
+++ R/state.R
@@ -24,25 +24,3 @@ State <- function(packages, date) {
 ReadDataFile <- function(filename) {
   if (file.exists(filename)) readRDS(filename)
 }
-
-StateData <- function(cran, datadir, params=list()) {
-  date <- Date(cran$packages, params$package, params$version, params$date)
-  state <- State(cran$packages, date)
-  if (is.null(params[["package"]])) {
-    list(date=date, state=state)
-  } else {
-    package <- params$package
-    versions <- cran$packages[cran$packages$package == package, ]
-    version <- state[state$package == package, ]$version
-    filename <- sprintf("%s_%s.rds", package, version)
-    descfile <- ReadDataFile(file.path(datadir, "descfiles", filename))
-    deps <- DependencyGraph(cran$deps, state)
-    code <- ReadDataFile(file.path(datadir, "functions", filename))
-    clones <- Clones(package, version, cran)
-    namespace <- ReadDataFile(file.path(datadir, "namespaces", filename))
-    conflicts <- Conflicts(package, version, cran)
-    list(date=date, packages=state, package=package, version=version,
-         versions=versions, descfile=descfile, deps=deps,
-         code=code, clones=clones, namespace=namespace, conflicts=conflicts)
-  }
-}
