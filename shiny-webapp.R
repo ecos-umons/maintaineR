20aa3c985b04ebfa49d419c4afdf6894091cb1e0
Maëlick Claes <himself@maelick.net>
2014-09-18 15:14:56 +0200

    Shiny-webapp script

--- shiny-webapp.R
+++ shiny-webapp.R
@@ -1,5 +1,4 @@
 library(shiny)
 datadir <- "/data/cran"
-## datadir <- "/home/maelick/sources/maintaineR/data2"
 allow.download <- TRUE
 shiny::runApp(system.file("webapp", package="maintaineR"), port=3000)
