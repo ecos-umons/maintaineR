a0fcf819182ca207c641ece9c6a13004e8aeb00b
Maëlick Claes <himself@maelick.net>
2014-09-18 14:33:26 +0200

    d3 sankey JS lib moved

--- inst/webapp/ui.R
+++ inst/webapp/ui.R
@@ -2,9 +2,9 @@ library(shiny)
 
 shinyUI(tagList(
   tags$head(
-    tags$link(rel="stylesheet", href="lib/rCharts_d3_sankey/libraries/widgets/d3_sankey/css/sankey.css"),
-    tags$script(src="lib/rCharts_d3_sankey/libraries/widgets/d3_sankey/js/d3.v3.js"),
-    tags$script(src="lib/rCharts_d3_sankey/libraries/widgets/d3_sankey/js/sankey.js"),
+    tags$link(rel="stylesheet", href="lib/d3sankey/css/sankey.css"),
+    tags$script(src="lib/d3sankey/js/d3.v3.js"),
+    tags$script(src="lib/d3sankey/js/sankey.js"),
     tags$style("
       .rChart {
         display: block;
