efd65d7be3d9afd4ba2dcde2d4e0093f365bd28b
Maëlick Claes <himself@maelick.net>
2014-11-05 14:40:12 +0100

    Bugfix in Date function

--- R/state.R
+++ R/state.R
@@ -1,11 +1,11 @@
-Date <- function(packages, package=NULL, version=NULL, date=NULL) {
-  if (is.null(date)) {
-    if (!is.null(package)) {
-      packages <- packages[package == package, ]
-      if (!is.null(version)) packages <- packages[version == version, ]
+Date <- function(packages, p=NULL, v=NULL, d=NULL) {
+  if (is.null(d)) {
+    if (!is.null(p)) {
+      packages <- packages[package == p]
+      if (!is.null(version)) packages <- packages[version == v]
     }
     as.Date(max(packages$mtime))
-  } else as.Date(date)
+  } else as.Date(d)
 }
 
 State <- function(packages, date=Sys.Date()) {
